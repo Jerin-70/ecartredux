c5c3fcfc347f8b84b6896334c1002a7bbfccede1
Jerin-77 <jerinjohn1202@gmail.com>
2025-08-26 12:54:16 +0530

    Wishlist compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { Route, Routes } from "react-router-dom"
+import Wishlist from "./pages/Wishlist"
+import View from "./pages/View"
+import Pnf from "./pages/Pnf"
+import Cart from "./pages/Cart"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 function App() {
-  
+
 
   return (
-    <>
-      <h1 className="text-6xl text-amber-300">Ecart</h1>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/wishlist" element={<Wishlist />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/:id/view" element={<View />}></Route>
+        <Route path="/*" element={<Pnf />}></Route>
+
+
+
+
+
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
 
